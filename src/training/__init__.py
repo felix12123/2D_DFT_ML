improve kernel plotting, fix discretisation in potential calculation

src/training/__init__.py
@@ -29,7 +29,7 @@ def estimate_num_batches(datafolder, batchsize, traintest_split=0.8, L=10, dx=0.
 
 # define class to contain the model, optimizer, loss function, metric function etc
 class MLTraining:
-    def __init__(self, folder, model, L, dx, datafolder, criterion=nn.MSELoss(), optimizer_class=torch.optim.Adam, metric=nn.L1Loss(), trained_epochs=0, windowed=True, batchsize=64, shuffle=True, num_workers=4, traintest_split=0.8, save_interval=1, lr_decay=None, batch_lr_decay=1-2e-5, lr_start=3e-4, min_lr=0, device=None, total_batches=None):
+    def __init__(self, folder:str, model:nn.Module, L:float, dx:float, datafolder:str, criterion=nn.MSELoss(), optimizer_class=torch.optim.Adam, metric=nn.L1Loss(), trained_epochs:int=0, windowed:bool=True, batchsize:int=64, shuffle:bool=True, num_workers:int=4, traintest_split:float=0.8, save_interval=1, lr_decay:float=None, batch_lr_decay:float=1-2e-5, lr_start:float=3e-4, min_lr:float=0, device=None, total_batches:int=None):
         """
         Initialize the MLTraining class. This class contains the model, optimizer, loss function, metric function, and other parameters for training the model.
         It also contains methods for training the model, logging the training progress, and saving the model.
@@ -63,7 +63,7 @@ class MLTraining:
         if device is None:
             device = "cuda" if torch.cuda.is_available() else "cpu"
         if not os.path.exists(datafolder):
-            raise ValueError("Data folder does not exist")
+            raise ValueError(f"Data folder {datafolder} does not exist")
             
         ### default values ###
         # check if model is constructor

src/training/_plotting.py
@@ -564,8 +564,19 @@ def collect_all_kernels(conv2d_layer:nn.Conv2d):
     return kernels
 
 
-def show_kernels(self, output_file='', num_kernels=None, conv_layer=0, title=""):
-    # Ensure the model is on the CPU
+def show_kernels(self, output_file=None, num_kernels=None, conv_layer=None, title=""):
+    if output_file is None:
+        output_file = self.workspace + "/plots/kernels.pdf"
+    
+    if conv_layer is None:
+        conv_layers = range(len(self.model.collect_all_kernels()))
+        output = []
+        for i in conv_layers:
+            filename = output_file.split(".")[0] + f"_{i+1}." + output_file.split(".")[-1]
+            out = self.show_kernels(output_file=filename, num_kernels=num_kernels, conv_layer=i, title=title)
+            output.append(out)
+        return output
+    
     # Get the first convolutional layer
     kernels = self.model.collect_all_kernels()[conv_layer]
     
@@ -577,7 +588,7 @@ def show_kernels(self, output_file='', num_kernels=None, conv_layer=0, title="")
     
     # Plot the kernel matrix
     plt.rc("font", size=10)
-    fig = plt.figure(figsize=(6, 6), facecolor=(1,1,1,0))
+    fig = plt.figure(figsize=(6, 6), facecolor=(1,1,1,0), dpi=300)
     max_diff = max(abs(kernel_matrix[np.isfinite(kernel_matrix)].min()), abs(kernel_matrix[np.isfinite(kernel_matrix)].max()))
     norm = TwoSlopeNorm(vmin=-max_diff, vcenter=0, vmax=max_diff)
     plt.imshow(kernel_matrix, cmap='coolwarm', norm=norm)

src/training/_simfolder_utils.py
@@ -141,8 +141,8 @@ def calculate_Vext(datafolder:str, i:int, n:int=7):
     L = params["L"]
     dx = params["dx"]
     
-    l = int(L/dx)
-    dx_large = dx/n # dx of the large grid
+    l = int(np.ceil((L/dx)))
+    dx_large = L/(l*n) # dx of the large grid
     
     xs = np.linspace(dx_large/2, L-dx_large/2, l*n)
     X, Y = np.meshgrid(xs, xs)

src/training/models/_FMT.py
@@ -9,7 +9,7 @@ from ._baseclass import BaseModel
 # In practice, it is easier to implement this by flipping the input tensor before and after the second convolution.
 class Model_FMT(BaseModel):
     """Model that is strongly inspired by the FMT definition of c_1"""
-    def __init__(self, Nd, Nn=16, hidden_channels=64, hidden_layers=6, convtype=nn.Conv2d, activation=Swish):
+    def __init__(self, Nd:int, Nn:int=16, hidden_channels:int=64, hidden_layers:int=6, convtype=nn.Conv2d, activation=Swish):
         """Initializes a Model_FMT object
 
         Args: